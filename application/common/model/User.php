37b4a60c5cd41affd35b071844ff8cef026a3147
jackytallow <3434481891@qq.com>
2019-02-04 16:17:35 +0800

    用户模型基本设置

--- application/common/model/User.php
+++ application/common/model/User.php
@@ -14,5 +14,32 @@ use think\Model;
 class User extends Model
 {
 
-      protected $pk = 'id';
+      protected $pk = 'id'; //默认主键
+      protected $table = 'zh_user'; //默认数据表
+
+      protected $autoWriteTimestamp = true;//开启自动时间戳
+    //定义时间戳字段名，默认为create_time和create_time，如果一致可省略
+    //如果想关闭某个时间戳字段，将值设置为false即可，$creat_time= false
+   protected $createTime = 'create_time';
+   protected $updateTime = 'update_time';
+   protected $dataFormat = 'Y年m月d日';
+
+   //用户状态获取器
+    public function getStatusAttr($value){
+
+        $status = ['1'=>'启用','0'=>'禁用'];
+        return $status[$value];
+    }
+
+//    //用户类型获取器
+//    public function getIsAdminAttr($value){
+//        $status = ['1'=>'管理员','0'=>'注册会员'];
+//        return $status[$value];
+//    }
+
+    //用户密码修改器
+    public function setPasswordAttr($value)
+    {
+      return sha1($value);
+    }
 }
