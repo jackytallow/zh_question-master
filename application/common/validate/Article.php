833b75d91c21d11c0166ac1239050851ecce055e
jackytallow <3434481891@qq.com>
2019-02-05 13:00:03 +0800

    编写发布文章前端页面

--- application/common/validate/Article.php
+++ application/common/validate/Article.php
@@ -16,6 +16,8 @@ class Article extends Validate
 
     protected $rule = [
         'title|标题' => 'require|length:5,50',
-        'content|文章内容'=>'require|length'
-    ]
+        'content|文章内容'=>'require|length:5,10000',
+        'user_id|作者' => 'require',
+        'cate_id|栏目名称' => 'require'
+    ];
 }
