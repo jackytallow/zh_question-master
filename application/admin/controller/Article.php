ee3eff9cd6e24dea6266cd559cfcd79a1a5e2927
jackytallow <3434481891@qq.com>
2019-02-06 16:32:13 +0800

    编写文章编辑与删除功能

--- application/admin/controller/Article.php
+++ application/admin/controller/Article.php
@@ -8,8 +8,9 @@
 
 namespace app\admin\controller;
 
-
+use app\admin\common\model\Article as ArtModel;
 use app\admin\common\controller\Base;
+use think\facade\Request;
 
 class Article extends Base
 {
@@ -53,6 +54,75 @@ class Article extends Base
     }
 
 
+   //渲染文章编辑界面
+    public function artEdit()
+    {
+        //1.获取要编辑的文章主键
+        $artId = Request::param('id');
 
+        //2.根据主键查询到需要更新的用户全部信息
+        $artInfo = ArtModel::where('id',$artId)->find();
+
+        //3.获取到所有的分类信息
+        $cateList = Cate::all();
+
+        //4.设置编辑界面的模板变量
+        $this->view->assign('title','编辑文章');
+        $this->view->assign('artInfo',$artInfo);
+        $this->view->assign('cateList',$cateList);
+
+        //5.渲染编辑界面
+        return $this->fetch('artedit');
+    }
+
+
+    //处理文章编辑操作
+    public function doEdit()
+    {
+
+        //1.获取表单提交的数据
+        $data = Request::param();
+
+        //2.获取上传的标题图片信息
+        $file = Request::file('title_img'); //获取file对象
+
+        //3.文件信息验证与上传大奥服务器指定目录
+        $info = $file -> validate([
+            'size'=>5000000000,  //文件大小
+            'ext'=>'jpeg,jpg,png,gif'  //文件扩展名
+        ]) -> move('uploads/');  //移动到public/uploads目录下面
+
+
+        //4.判断上传文件的信息
+        if ($info) {
+            $data['title_img'] = $info->getSaveName();
+        } else {
+            $this->error($file->getError());
+        }
+
+
+        //5.将数据写到文档表中
+        if(ArtModel::update($data)){ //条件中$data['id']中
+            $this->success('文章更新成功','artList');
+        } else {
+            $this->error('文章更新失败');
+        }
+    }
+
+
+    //执行文章删除操作
+    public function doDelete()
+    {
+        //1.获取要删除的文章ID
+        $artId = Request::param('id');
+
+        //2.执行删除操作并判断是否成功
+        if (ArtModel::destroy($artId)){
+            $this->success('删除成功');
+        }
+
+        //3.如果删除失败
+        $this->error('删除失败');
+    }
 
 }
